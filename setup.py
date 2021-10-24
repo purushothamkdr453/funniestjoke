fc4f92281ace3645132b446efb7ffc0631d54891
purushotham <purushothamkdr453@gmail.com>
2021-10-24 11:51:23 +0530

    updated version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='funniestjoke',
-      version='0.1',
+      version='0.2',
       description='The funniest joke in the world',
       url='http://github.com/purushothamkdr453/funniestjoke',
       author='purushotham reddy',
